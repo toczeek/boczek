0a522fc34d99b5efa99be87f4fed975936d8ed8d
Eryk Rutkowski <erykrutkowski@gmail.com>
2017-01-08 22:15:35 +0100

    WIP

--- app/src/main/java/com/example/toczek/wrumwrum/Fragments/DetailsFragment.java
+++ app/src/main/java/com/example/toczek/wrumwrum/Fragments/DetailsFragment.java
@@ -5,18 +5,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.toczek.wrumwrum.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Toczek on 2017-01-07.
  */
 
 public class DetailsFragment extends Fragment {
+    @BindView(R.id.details_fragment_air_temp)
+    TextView mAirTempTv;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.details_fragement, container, false);
+        View view = inflater.inflate(R.layout.details_fragement, container, false);
+        ButterKnife.bind(view);
+        return view;
     }
 }
